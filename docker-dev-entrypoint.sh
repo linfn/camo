c8af26cb37c4a9b8e09b890a5f4c6a62015a6240
linfn <linfeng.lu@hotmail.com>
2019-08-28 16:03:36 +0800

    add docker IPv6 support

--- docker-dev-entrypoint.sh
+++ docker-dev-entrypoint.sh
@@ -6,4 +6,12 @@ export GOPROXY=https://goproxy.io
 
 go get -v ./...
 
-exec camo-server -ip4 10.20.0.1/24 -nat4 -autocert-host "$CAMO_HOST" -pprof -log-level "$CAMO_LOG_LEVEL"
+if [ -n "$CAMO_IP4" ]; then
+    IP4FLAGS="-ip4 $CAMO_IP4 -nat4"
+fi
+
+if [ -n "$CAMO_IP6" ]; then
+    IP6FLAGS="-ip6 $CAMO_IP6 -nat6"
+fi
+
+exec camo-server $IP4FLAGS $IP6FLAGS -autocert-host "$CAMO_HOST" -pprof -log-level "$CAMO_LOG_LEVEL"
